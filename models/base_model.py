07d4774b9ee9777dc0e35d724176fe36798d0e27
root <Bbr011>
2024-03-09 08:45:56 -0800

    basemodelv2

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,26 @@ class BaseModel:
 
     """the base class"""
 
-    def __init__(self):
-        """Initializing the instance attributes"""
+    def __init__(self, *args, *kwargs):
+        """Initializing the instance attributes
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of keyvalues arguments
+        """
+
+        if kwargs != {} and kwargs is not None:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """function that returns string representation"""
