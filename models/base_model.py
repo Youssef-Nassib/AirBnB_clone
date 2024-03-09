10009f6ac7839e7f8faadcc45bde8593287bd5c3
root <Bbr011>
2024-03-09 07:32:24 -0800

    basemodel

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""the base module"""
+from datetime import datetime
+
+
+class BaseModel:
+    """the base class"""
+
+    def __init__(self):
+        """Initializing the instance attributes"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """function that returns string representation"""
+
+        return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+
+    def save(self):
+        """updates the public instance attribute with the current datetime"""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of the instance"""
+
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = type(self).__name__
+        obj_dict["created_at"] = my_dict["created_at"].isoformat()
+        obj_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return obj_dict
