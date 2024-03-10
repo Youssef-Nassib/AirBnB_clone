49189293357e1fb6b60a4e24c074ebeaf91ac28e
root <Bbr011>
2024-03-10 07:29:26 -0700

    creatconsole

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """console module"""
 import cmd
+from models.base_model import BaseModel
 import re
 
 
@@ -25,6 +26,25 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """does nothing when empty line is inserted"""
         pass
+
+    def so_creat(self, arg):
+        """Creates a new instance of BaseModel"""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        class_name = arg.split()[0]
+        if class_name not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+
+        new_instance = BaseModel()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, line):
+        """Prints string representation of instance based on class name and id"""
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
